535b0f0a8903ada01273f4a5dff5de3150933556
Colin Mackay <mackay_colin@hotmail.com>
2017-05-17 23:01:35 +0100

    Add project files.

---
+++ test/DocFX.Plugin.PlantUML.Test/PlantUMLConverterTests.cs
@@ -0,0 +1,68 @@
+﻿using docfx_plantuml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Svg;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DocFX.Plugin.PlantUML.Test
+{
+    [TestClass]
+    public class PlantUMLConverterTests
+    {
+        static PlantUMLConverter _converter = new PlantUMLConverter("https://www.plantuml.com/");
+
+        [TestMethod]
+        public void ConvertToImage_PlantUML_ReturnPNG()
+        {
+            var plantUML = "Bob -> Alice : hello";
+
+            Task<Stream> response = _converter.ConvertToImage(plantUML, RenderFormat.PNG);
+
+            var pngImg = Image.FromStream(response.Result);
+
+            Assert.AreEqual<ImageFormat>(ImageFormat.Png, pngImg.RawFormat);
+        }
+
+        [TestMethod]
+        public void ConvertToImage_PlantUML_ReturnSVG()
+        {
+            var plantUML = "Bob -> Alice : hello";
+
+            Task<Stream> response = _converter.ConvertToImage(plantUML, RenderFormat.SVG);
+
+            var svgImg = SvgDocument.Open<SvgDocument>(response.Result);
+
+            Assert.IsNotNull(svgImg);
+        }
+
+        [TestMethod]
+        public void ConvertToImage_PlantUML_ReturnASCIIArt()
+        {
+            var plantUML = "Bob -> Alice : hello";
+
+            Task<Stream> response = _converter.ConvertToImage(plantUML, RenderFormat.TXT);
+
+            using (var reader = new StreamReader(response.Result))
+            {
+                var output = reader.ReadToEnd();
+                Assert.IsTrue(output.Length > 0);
+            }
+        }
+
+        [TestMethod]
+        public void ConvertToImage_PlantUML_ReturnImageMap()
+        {
+            var plantUML = "participant Bob [[http://www.plantuml.com]]\nBob->Alice : [[http://plantuml.sourceforge.net]] hello";
+
+            Task<Stream> response = _converter.ConvertToImage(plantUML, RenderFormat.MAP);
+
+            using (var reader = new StreamReader(response.Result))
+            {
+                var output = reader.ReadToEnd();
+                Assert.IsTrue(output.StartsWith("<map"));
+            }
+        }
+    }
+}
---
+++ test/DocFX.Plugin.PlantUML.Test/PlantUMLEncoderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using docfx_plantuml;
+using System.Text;
+
+namespace DocFX.Plugin.PlantUML.Test
+{
+    [TestClass]
+    public class PlantUMLEncoderTests
+    {
+        [TestMethod]
+        public void EncodePlantUML_UMLString_ReturnsValidEncodedString()
+        {
+            var plantUML = "Bob -> Alice : hello";
+            var encodedUML = "SyfFKj2rKt3CoKnELR1Io4ZDoSa70000";
+
+            Assert.AreEqual<string>(encodedUML, plantUML.ToString().EncodePlantUML());
+        }
+
+        [TestMethod]
+        public void EncodePlantUML_UMLMultiLineString_ReturnsValidEncodedString()
+        {
+            var plantUML = new StringBuilder();
+            plantUML.AppendLine("Bob -> Alice : hello");
+            plantUML.AppendLine("Alice -> Bob : Hi");
+
+            var encodedUML = "SyfFKj2rKt3CoKnELR1Io4ZDoSddWl20mav0MIi5Zqm0";
+
+            Assert.AreEqual<string>(encodedUML, plantUML.ToString().EncodePlantUML());
+        }
+
+    }
+}
